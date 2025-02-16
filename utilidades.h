4e0b6ff212d74394711190b7153306244d637e57
Sofia Prata <158978435+sofiaprata@users.noreply.github.com>
2025-02-15 21:21:38 -0300

    Create utilidades.h

---
+++ utilidades.h
@@ -0,0 +1,8 @@
+// utilidades.h
+#ifndef UTILIDADES_H
+#define UTILIDADES_H
+
+void criarPastaDestino(const char *pasta);
+void processarPasta(const char *pasta, const char *pasta_destino);
+
+#endif
