46c0f1fbf4279575c0ba4e1a615bc3985159e99a
Sofia Prata <158978435+sofiaprata@users.noreply.github.com>
2025-02-16 16:13:05 -0300

    Update and rename utilidades.h to processamento.h

--- utilidades.h
+++
@@ -1,8 +0,0 @@
-// utilidades.h
-#ifndef UTILIDADES_H
-#define UTILIDADES_H
-
-void criarPastaDestino(const char *pasta);
-void processarPasta(const char *pasta, const char *pasta_destino);
-
-#endif
